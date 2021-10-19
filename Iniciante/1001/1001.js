879bc442355351851b98cf5873f4646375032eda
Rodrigo Queiroz <rodrigojosequeiroz@gmail.com>
2021-10-18 23:00:08 -0300

    cria pasta 1001

---
+++ Iniciante/1001/1001.js
@@ -0,0 +1,13 @@
+// comando que imprime mensagem no console do navegador ou do vscode
+console.log('URI Exercício')
+
+// comando que exibe um alerta no navegador
+alert('URI Exercício')
+
+// comando que escreve no documento html
+document.write('Bagage')
+
+// comando para recuperar dados digitados pelo usuário
+// obs: o retorno é sempre uma string
+let nome = prompt('Digite seu nome: ')
+console.log(nome)
